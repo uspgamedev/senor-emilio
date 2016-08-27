aa05897b93507dc2dae04f9faef5501118e3886c
Rafael Correia <rafaeljpc@gmail.com>
2016-08-27 03:35:19 -0300

    Portals

---
+++ scripts/classes/portal.gd
@@ -0,0 +1,33 @@
+extends Node2D
+
+export(NodePath) var portal_link = null
+
+onready var storage = get_node("Storage")
+
+func get_portal_link():
+  return get_node(portal_link)
+
+func is_full():
+  if storage.get_child_count() > 0:
+    return true
+  return false
+
+func store(item):
+  if is_full():
+    return
+  storage.add_child(item)
+  yield(get_tree(), "fixed_frame")
+  get_portal_link().store(item)
+
+func remove():
+  var item = get_item()
+  if item == null:
+    return
+  storage.remove_child(item)
+  yield(get_tree(), "fixed_frame")
+  get_portal_link().remove(item)
+
+func get_item():
+  if not is_full():
+    return null
+  return storage.get_child(0)
