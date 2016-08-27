20f1e19960116f2fdf50e1334067d7d8981ccd6b
Kazuo256 <kazuo256@gmail.com>
2016-08-27 10:36:22 -0300

    Door between stages, close #6

--- scripts/classes/door.gd
+++ scripts/classes/door.gd
@@ -3,13 +3,23 @@ extends StaticBody2D
 onready var closed = get_node("closed")
 onready var opened = get_node("opened")
 
+export(int) var target_stage = 0
+export(String) var target_door = "door"
+
+signal change_stage(which, door)
+
 func on():
   print("door open")
   opened.show()
   closed.hide()
-  
+
 
 func off():
   print("door closed")
   opened.hide()
   closed.show()
+
+func interact(body):
+  #emit_signal("change_stage", target_stage,  target_door)
+  get_node("/root/main/gameplay")._change_stage(target_stage, target_door)
+  printt(get_name(), "door used!")
--- scripts/classes/pickable.gd
+++ scripts/classes/pickable.gd
@@ -1,5 +1,8 @@
 
 extends "res://scripts/classes/body.gd"
 
+func _ready():
+  print("box spawned")
+
 func interact(body):
   body.grab(self)
