9b298c51e38be33a2344a93fddfcb4dd7206b063
Kazuo256 <kazuo256@gmail.com>
2016-08-27 02:54:22 -0300

    Dual map mechanic
    Change gameplay state to reflect time duality

--- scripts/states/gameplay.gd
+++ scripts/states/gameplay.gd
@@ -1,25 +1,59 @@
 
 extends Node2D
 
-var _hero
+const FUTURE = 0
+const PAST = 1
+
+export(int, "FUTURE","PAST") var current = FUTURE
+
+onready var _maps = [
+  get_node("future"),
+  get_node("past")
+]
+onready var _chars = [
+  _maps[FUTURE].get_node("bodies/human"),
+  _maps[PAST].get_node("bodies/cat")
+]
+onready var _cam = Camera2D.new()
 
 func _ready():
     var input = get_node("/root/input")
-    self._hero = get_node("map/bodies/hero")
     _start_camera()
-    input.connect("hold_direction", self._hero, "_move_to")
+    _connect_state()
     input.connect("press_quit", self, "_quit")
-    input.connect("press_action", self._hero, "_act")
-    self._hero.connect("died", self, "_quit")
     set_process(true)
 
+func _connect_state():
+  input.connect("hold_direction", get_current_char(), "_move_to")
+  input.connect("press_action", get_current_char(), "_act")
+  get_current_map().show()
+  get_current_char().add_child(_cam)
+
+func _disconnect_state():
+  input.disconnect("hold_direction", get_current_char(), "_move_to")
+  input.disconnect("press_action", get_current_char(), "_act")
+  get_current_map().hide()
+  get_current_char().remove_child(_cam)
+
 func _start_camera():
-    var cam = Camera2D.new()
-    self._hero.add_child(cam)
-    cam.make_current()
+  _cam.make_current()
 
 func _quit():
     get_tree().quit()
 
-func get_hero():
-    return self._hero
+func get_current_map():
+  return _maps[current]
+
+func get_current_char():
+  return _chars[current]
+
+func get_other_map():
+  return _maps[1 - current]
+
+func get_other_char():
+  return _chars[1 - current]
+
+func switch_time_state():
+  disconnect_state()
+  current = 1 - current
+  connect_state()
