23da85f6fb9bf146ee88a10478c68706fb864bb0
Kazuo256 <kazuo256@gmail.com>
2016-08-28 00:07:18 -0300

    No more door teleport

--- scripts/classes/door.gd
+++ scripts/classes/door.gd
@@ -9,8 +9,6 @@ onready var lock = get_node("lock")
 onready var anim = get_node("lock/AnimationPlayer")
 onready var teleport = get_node("teleport")
 
-export(int) var target_stage = 0
-export(String) var target_door = "door"
 export(String) var key_required = null
 export(bool) var start_opened = true
 
@@ -32,7 +30,6 @@ func open():
   anim.queue("opened")
   deactivate_collision()
   _open = true
-  set_fixed_process(true)
 
 func close():
   print("door closed")
@@ -40,7 +37,6 @@ func close():
   anim.queue("closed")
   activate_collision()
   _open = false
-  set_fixed_process(false)
 
 func is_closed():
   return anim.get_current_animation() != "opened"
@@ -60,21 +56,7 @@ func get_spawn_pos():
 func off():
   close()
 
-func _fixed_process(dt):
-  var bodies = teleport.get_overlapping_bodies()
-  if bodies.size() > 0:
-    for body in bodies:
-      if body.has_method("get_layer_mask") and body.get_layer_mask() == teleport.get_collision_mask():
-        _teleport(body)
-
-func _teleport(body):
-  printt(get_path(), "teleport!", body.get_path())
-  if is_inside_tree() and body.get_script() == Hero and anim.get_current_animation() == "opened":
-    printt(get_path(), "door used!", target_stage, target_door)
-    get_node("/root/main/gameplay")._change_stage(target_stage, target_door)
-
 func interact(body):
-  #emit_signal("change_stage", target_stage,  target_door)
   if is_closed() and body.get_pocket_item() != null:
     if unlock(body.get_pocket_item()):
       body.drop()
