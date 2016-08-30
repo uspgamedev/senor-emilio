bf1d7577ccc1d070e319b12da8b38a5524c8d28f
Eduardo Silva Araújo <duduktamg@hotmail.com>
2016-08-29 21:51:31 -0300

    Add preview to portal

--- scripts/classes/pickable.gd
+++ scripts/classes/pickable.gd
@@ -9,3 +9,6 @@ func interact(body):
   var collide = self.get_layer_mask() & body.get_collision_mask()
   if collide != 0:
     body.grab(self)
+
+func get_texture():
+  return get_node("sprite").get_texture()
